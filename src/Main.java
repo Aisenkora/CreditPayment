dd496125b099370e00bc64e8c9abbb5e2fd02fb2
Aesonne <radius.05-good@icloud.com>
2024-04-24 01:57:00 +0300

    Last change

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,11 @@ public class Main {
         CreditPaymentService service = new CreditPaymentService();
         double percent =9.99; //процентная ставка по кредиту (%)
         int amount = 1_000_000; //сумма кредита (руб.)
-        int months = 36; //срок кредита (мес.)
+        int months = 12; //срок кредита (мес.)
 
-        double payment = service.calculate(percent, amount, months);
-        System.out.println(payment);
+        System.out.println(service.calculate(percent, amount, months));
+        System.out.println(service.calculate(9.99, 1000000, 24));
+        System.out.println(service.calculate(9.99, 1000000, 36));
     }
 }
 
