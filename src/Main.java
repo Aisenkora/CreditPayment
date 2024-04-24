527aedc41f907bc126397498393702ddb2fd1496
Aesonne <radius.05-good@icloud.com>
2024-04-24 21:40:27 +0300

    Refinement

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         CreditPaymentService service = new CreditPaymentService();
-        double percent =9.99; //процентная ставка по кредиту (%)
+        double percent = 9.99; //процентная ставка по кредиту (%)
         int amount = 1_000_000; //сумма кредита (руб.)
         int months = 12; //срок кредита (мес.)
 
